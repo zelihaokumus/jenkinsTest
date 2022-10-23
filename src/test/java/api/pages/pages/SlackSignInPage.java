66d220db1f540b4b8a2c42c95a603feea88e2931
zelihaokumus <okumuszeliha100@gmail.com>
2022-10-22 23:12:08 -0700

    .JenkinsFile

--- src/test/java/api/pages/pages/SlackSignInPage.java
+++ src/test/java/api/pages/pages/SlackSignInPage.java
@@ -1,5 +1,6 @@
 package api.pages.pages;
 
+import groovyjarjarantlr.collections.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
